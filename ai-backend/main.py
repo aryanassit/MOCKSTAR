8efde77385189c45e5c8ad04cb0a087209d81698
Aryan <aryanassit@gmail.com>
2026-07-17 09:23:23 +0530

    Update main.py

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -23,8 +23,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://mockstar-7414.vercel.app",
-        "https://mockstar-7414-git-main-aryanassits-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
